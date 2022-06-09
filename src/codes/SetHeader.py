b8c5f37605d4aa7126fdfe9b3b2c73705b985c3c
CountrySideEngineer <deep-sleeper@nifty.com>
2022-06-10 07:32:32 +0900

    Modify code. -Update code to use function to set headers as argument, function-pointer. -Remove "import" code not used anymore.

--- src/codes/SetHeader.py
+++ src/codes/SetHeader.py
@@ -1,10 +1,29 @@
-from ast import arg
-from email import headerregistry
 import sys
-import os
 import openpyxl
 from openpyxl import worksheet
 from openpyxl.worksheet.header_footer import _HeaderFooterPart
+
+def HeaderFuncPointer(path:str, headers:list, header_func) -> None:
+	"""Set header of excel file.
+	
+	Set header of all sheets in a excel file.
+
+	Args:
+		path(str): Path to file to set header.
+		headers(list): Collection of strings to set into header.
+						One item is one line in header.
+						When output, all items are joined by change line code.
+		header_func: Function used to set header.	
+	
+	"""
+	wb = openpyxl.load_workbook(path)
+
+	sheets = wb.worksheets
+	for sheet in sheets:
+		header_func(sheet=sheet, headers=headers)
+
+	wb.save(path)
+	wb.close()
 
 def SetHeaderRight(path:str, headers:list) -> None:
 	"""Set header of right.
@@ -13,18 +32,12 @@ def SetHeaderRight(path:str, headers:list) -> None:
 
 	Args:
 		path(str): Path to file to set header.
-		headetrs(list): Collection of strings to set into header.
+		headers(list): 	Collection of strings to set into header.
 						One item is one line in header.
 						When output, all items are joined by change line code.
 	"""
-	wb = openpyxl.load_workbook(path)
-
-	sheets = wb.worksheets
-	for sheet in sheets:
-		SetPageHeaderRight(sheet=sheet, headers=headers)
-
-	wb.save(path)
-	wb.close()
+	header_func = SetPageHeaderRight
+	HeaderFuncPointer(path=path, headers=headers, header_func=header_func)
 
 def SetPageHeaderRight(sheet:worksheet, headers:list) -> None:
 	"""Set header of right.
@@ -33,7 +46,7 @@ def SetPageHeaderRight(sheet:worksheet, headers:list) -> None:
 
 	Args:
 		sheet(worksheet): Worksheet object to set header.
-		headetrs(list): Collection of strings to set into header.
+		headers(list):	Collection of strings to set into header.
 						One item is one line in header.
 						When output, all items are joined by change line code.
 	"""
@@ -47,18 +60,12 @@ def SetHeaderLeft(path:str, headers:list) -> None:
 
 	Args:
 		path(str): Path to file to set header.
-		headetrs(list): Collection of strings to set into header.
+		headers(list):	Collection of strings to set into header.
 						One item is one line in header.
 						When output, all items are joined by change line code.
 	"""
-	wb = openpyxl.load_workbook(path)
-
-	sheets = wb.worksheets
-	for sheet in sheets:
-		SetPageHeaderLeft(sheet=sheet, headers=headers)
-
-	wb.save(path)
-	wb.close()
+	header_func = SetPageHeaderLeft
+	HeaderFuncPointer(path=path, headers=headers, header_func=header_func)
 
 def SetPageHeaderLeft(sheet:worksheet, headers:list) -> None:
 	"""Set header of left.
@@ -67,13 +74,12 @@ def SetPageHeaderLeft(sheet:worksheet, headers:list) -> None:
 
 	Args:
 		sheet(worksheet): Worksheet object to set header.
-		headetrs(list): Collection of strings to set into header.
+		headers(list):	Collection of strings to set into header.
 						One item is one line in header.
 						When output, all items are joined by change line code.
 	"""
 	header_part = sheet.oddHeader.left
 	SetHeader(header_part=header_part, headers=headers)
-
 
 def SetHeaderCenter(path:str, headers:list) -> None:
 	"""Set header of center.
@@ -82,18 +88,12 @@ def SetHeaderCenter(path:str, headers:list) -> None:
 
 	Args:
 		path(str): Path to file to set header.
-		headetrs(list): Collection of strings to set into header.
+		headers(list):	Collection of strings to set into header.
 						One item is one line in header.
 						When output, all items are joined by change line code.
 	"""
-	wb = openpyxl.load_workbook(path)
-
-	sheets = wb.worksheets
-	for sheet in sheets:
-		SetPageHeaderCenter(sheet=sheet, headers=headers)
-
-	wb.save(path)
-	wb.close()
+	header_func = SetPageHeaderCenter
+	HeaderFuncPointer(path=path, headers=headers, header_func=header_func)
 
 def SetPageHeaderCenter(sheet:worksheet, headers:list) -> None:
 	"""Set header of center.
@@ -102,7 +102,7 @@ def SetPageHeaderCenter(sheet:worksheet, headers:list) -> None:
 
 	Args:
 		sheet(worksheet): Worksheet object to set header.
-		headetrs(list): Collection of strings to set into header.
+		headers(list):	Collection of strings to set into header.
 						One item is one line in header.
 						When output, all items are joined by change line code.
 	"""
@@ -115,7 +115,7 @@ def JoinHeaderText(headers:list) -> str:
 	Join strings by change line code as headers
 
 	Args:
-		headetrs(list): Collection of strings to set into header.
+		headers(list):	Collection of strings to set into header.
 						One item is one line in header.
 						When output, all items are joined by change line code.
 
@@ -137,7 +137,7 @@ def SetHeader(header_part:_HeaderFooterPart, headers:list) -> None:
 
 	Args:
 		header_part(_HeaderFooterPart): _HeaderFooterPart object to set the headers.
-		headetrs(list): Collection of strings to set into header.
+		headers(list):	Collection of strings to set into header.
 						One item is one line in header.
 						When output, all items are joined by change line code.
 
