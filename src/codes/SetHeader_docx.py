610cb36190543dadf66d14bc05947899bda8e602
CountrySideEngineer <deep-sleeper@nifty.com>
2022-06-18 06:27:19 +0900

    Add code to set header into docx file.

---
+++ src/codes/SetHeader_docx.py
@@ -0,0 +1,37 @@
+from operator import le
+import os
+from pydoc import doc
+import sys
+import tarfile
+import docx
+from docx import Document
+
+def SetHeaderRight(path:str, headers:list) -> None:
+	document = Document(path)
+	sections = document.sections
+	section_header = sections[0].header
+
+	RemoveHeader(header_section=section_header)
+
+	for header_item in headers:
+		section_header.add_paragraph(header_item)
+
+	document.save(path)
+
+def RemoveHeader(header_section:docx.section.Sections) -> None:
+	paragraphs = header_section.paragraphs
+	for paragraph in paragraphs:
+		element = paragraph._element
+		element.getparent().remove(element)
+		paragraph._p = paragraph._element = None
+
+
+if '__main__' == __name__:
+	path = sys.argv[1]
+
+	headers = [
+		'header_code_011',
+		'header_code_044',
+		]
+
+	SetHeaderRight(path, headers=headers)
