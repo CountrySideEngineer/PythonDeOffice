c3e772752edaf1f9dce07b26d348375814427776
CountrySideEngineer <deep-sleeper@nifty.com>
2022-10-08 21:35:05 +0900

    Update python codes to setup excel footer. Update by splitting code into multiple files, SetFooter_left.py, SetFooter_center.py, and SetFooter_right.py, according to footer position. The SetFooter.py code has only code to setup footer passed by each codes splited from it.

---
+++ src/codes/SetFooter_center.py
@@ -0,0 +1,45 @@
+import sys
+import openpyxl
+from openpyxl import worksheet
+#from openpyxl.worksheet.header_footer import _HeaderFooterPart
+import SetFooter as sf
+
+def SetFooter(path:str, footers:list) -> None:
+	"""Set footer of center.
+
+	Setup center footer of all sheets in a excel file.
+
+	Args:
+		path(str): Path to file to set header.
+		headetrs(list): Collection of strings to set into header.
+						One item is one line in header.
+						When output, all items are joined by change line code.
+	"""
+	wb = openpyxl.load_workbook(path)
+
+	sheets = wb.worksheets
+	for sheet in sheets:
+		SetFooterToSheet(sheet=sheet, footers=footers)
+
+	wb.save(path)
+	wb.close()
+
+def SetFooterToSheet(sheet:worksheet, footers:list) -> None:
+	"""Set footer of center.
+
+	Setup center footer of all sheets in a excel file.
+
+	Args:
+		sheet(worksheet): Worksheet object to set header.
+		headetrs(list): Collection of strings to set into header.
+						One item is one line in header.
+						When output, all items are joined by change line code.
+	"""
+	footer_part = sheet.oddFooter.center
+	sf.SetFooter(footer_part=footer_part, footers=footers)
+
+if '__main__' == __name__:
+	path = sys.argv[1]
+	argc = len(sys.argv)
+	footers = sys.argv[2:argc]
+	SetFooter(path=path, footers=footers)
