ed44400e6db1570e8004dd3616f84d0f506d61fe
CountrySideEngineer <deep-sleeper@nifty.com>
2023-04-15 10:33:45 +0900

    Add python files. Add files below: -ExcelFile.py -ExcelFileFooter.py, Concrete class methods to get footer part from worksheet as openpyxl object. -HeaderFooterItem.py, header or header content object. -OfficeFile.py -OfficeHeaderFooter.py, content about sheet name, header and footer.

---
+++ src/codes/Office/HeaderFooterItem.py
@@ -0,0 +1,9 @@
+import abc
+
+class HeaderFooterItem():
+	def __init__(self) -> None:
+		"""Default constructor.
+
+		"""
+		self.item = ""		# string object as header or foooter content.
+
---
+++ src/codes/Office/OfficeHeaderFooter.py
@@ -0,0 +1,12 @@
+import abc
+import HeaderFooterItem
+
+class OfficeHeaderFooter():
+	def __init__(self):
+		"""Constructor.
+		
+		"""
+		self.name = ""
+		self.headers = []
+		self.footers = []
+		
