973154e3e3a90cb6f6a0e0bf85f29a44fcc7a2d6
CountrySideEngineer <deep-sleeper@nifty.com>
2023-04-16 18:59:10 +0900

    Add python file. Add ExcelFileHeader.py, implementing ExcelFileHeader class, a class to access excel header.

---
+++ src/codes/Office/ExcelFileHeader.py
@@ -0,0 +1,39 @@
+import openpyxl
+from openpyxl import Workbook
+from openpyxl import worksheet
+from openpyxl.worksheet.header_footer import _HeaderFooterPart
+from ExcelFile import ExcelFile
+from OfficeHeaderFooter import OfficeHeaderFooter
+from HeaderFooterItem import HeaderFooterItem
+
+class ExcelFileHeader(ExcelFile):
+	def __init__(self, path : str = ""):
+		"""Constructor.
+		
+		"""
+		super().__init__(path)
+
+	def AppendItem(self, dst : OfficeHeaderFooter, item : HeaderFooterItem) -> None:
+		"""Append footer object as OffceHeaderFooter object to 
+		
+		"""
+		dst.headers.append(item)
+
+	def GetLeftPartFromSheet(self, sheet : worksheet) -> _HeaderFooterPart:
+		part = sheet.oddHeader.left
+		return part
+
+	def GetCenterPartFromSheet(self, sheet : worksheet) -> _HeaderFooterPart:
+		part = sheet.oddHeader.center
+		return part
+
+	def GetRightPartFromSheet(self, sheet : worksheet) -> _HeaderFooterPart:
+		part = sheet.oddHeader.right
+		return part
+
+if __name__ == '__main__':
+	path = r'E:\development\PythonDeOffice\src\samples\HeaderSample.xlsx'
+	header = ExcelFileHeader(path)
+
+	headers = header.Read()
+	print('footers')
