409154c3104b0501195b712abd542f4978a07c7e
CountrySideEngineer <deep-sleeper@nifty.com>
2023-04-19 22:24:43 +0900

    Update ExcelPageConfig class. Fix a bug in ExcelHeaderConfig class. -Miss spelling in class name. -Add code to import ExcelPageConfigCsvFormatter class.

--- src/codes/Office/ExcelPageConfig.py
+++ src/codes/Office/ExcelPageConfig.py
@@ -1,11 +1,13 @@
 import abc
 from OfficePageConfig import IOfficePageConfig
+from ExcelPageConfigCsvFormatter import ExcelPageConfigCsvFormatter as csv_formatter
 
-class ExcePageConfig(IOfficePageConfig):
+class ExcelPageConfig(IOfficePageConfig):
 	def __init__(self):
 		super().__init__
 
 		self.config = list
+		self.formatter = csv_formatter()
 
 	@abc.abstractclassmethod
 	def Import(self, path : str) -> None:
